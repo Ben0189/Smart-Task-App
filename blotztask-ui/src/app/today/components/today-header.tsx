5374137f8f2884d50ebae7ad176f4372cda9dc4b
unknown <xhma0211@gmail.com>
2024-12-31 20:34:02 +1100

    fix the comment

--- blotztask-ui/src/app/today/components/today-header.tsx
+++ blotztask-ui/src/app/today/components/today-header.tsx
@@ -1,18 +1,16 @@
 import { H1, H5 } from "@/components/ui/heading-with-anchor";
 import React from "react";
+import { format } from "date-fns";
 
 const TodayHeader = () => {
-  const date = new Date();
-  const dayName = date.toLocaleDateString("en-AU", { weekday: "long" });
-  const day = date.getDate();
-  const month = date.toLocaleDateString("en-AU", { month: "long" });
+  const todayDate = format(new Date(), "EEEE, d MMMM");
 
   return (
     <div className="flex flex-col gap-5">
       <H1 className="heading-primary">
         To<span className="heading-secondary">day</span>
       </H1>
-      <H5 className="text-gray-500 text-sm">{`${dayName}, ${day}. ${month}`}</H5>
+      <H5 className="text-gray-500 text-sm">{todayDate}</H5>
       <H5>List of today&apos;s tasks</H5>
     </div>
   );
