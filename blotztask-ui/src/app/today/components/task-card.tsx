10e1d08b656a60599745063691353cecfa0d9e2a
nxn-nicole <nxnnxn2@163.com>
2025-01-07 21:08:35 +1100

    Make the title and description wider

--- blotztask-ui/src/app/today/components/task-card.tsx
+++ blotztask-ui/src/app/today/components/task-card.tsx
@@ -22,9 +22,12 @@ const TaskCard = ({ task, handleCheckboxChange }) => {
             <p className="font-bold">{task.title}</p>
           </div>
 
-          <div className="flex flex-row justify-between w-full text-sm text-gray-500">
-            <p>{task.description}</p>
-            <div className="flex items-center">
+          <div className="flex flex-row w-full text-sm text-gray-500 mt-2">
+            <div className="flex flex-col w-full">
+              <p>{task.description}</p>
+            </div>
+
+            <div className="flex items-start ml-4 w-32">
               <div
                 className="h-4 w-4 rounded-full"
                 style={{ backgroundColor: task.label.color || 'gray' }}
