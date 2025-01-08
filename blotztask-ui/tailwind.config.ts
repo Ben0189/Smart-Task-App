1de6243dba4167d6787097ed654b06c684471096
benjaminneoh <benjaminneoh6343@gmail.com>
2025-01-08 19:59:02 +1100

    fix sidebar background

--- blotztask-ui/tailwind.config.ts
+++ blotztask-ui/tailwind.config.ts
@@ -88,7 +88,7 @@ const config: Config = {
 				'5': 'hsl(var(--chart-5))'
 			},
 			sidebar: {
-				DEFAULT: 'hsl(var(--sidebar-background))',
+				DEFAULT: '#EFF3FF',
 				foreground: 'hsl(var(--sidebar-foreground))',
 				primary: 'hsl(var(--sidebar-primary))',
 				'primary-foreground': 'hsl(var(--sidebar-primary-foreground))',
