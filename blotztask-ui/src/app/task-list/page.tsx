9371d397e36516623dc7ce7416742faafef1ea45
nxn-nicole <nxnnxn2@163.com>
2025-01-09 11:36:11 +1100

    Add handleCheckboxChange function to all task

--- blotztask-ui/src/app/task-list/page.tsx
+++ blotztask-ui/src/app/task-list/page.tsx
@@ -7,9 +7,11 @@ import Link from 'next/link';
 import { Trash } from 'lucide-react';
 import { DeleteDialog } from './components/delete-confirmation-dialog';
 import { H1 } from '@/components/ui/heading-with-anchor';
-
 import { TaskCard } from './components/task-card';
-import { fetchAllTaskItems } from '@/services/taskService';
+import {
+  completeTaskForToday,
+  fetchAllTaskItems,
+} from '@/services/taskService';
 import { TaskItemDTO } from '@/model/task-Item-dto';
 
 export default function Page() {
@@ -31,7 +33,7 @@ export default function Page() {
   }, []); // Runs on the first render using [] parameter and rerun when state changes, e.g add task
 
   const toggleDeleteTrigger = () => {
-    setIsDeleteTriggered((prev) => !prev); 
+    setIsDeleteTriggered((prev) => !prev);
   };
 
   const handleDialogClose = () => {
@@ -39,40 +41,55 @@ export default function Page() {
     setIsDeleteTriggered(false);
   };
 
+  const completeTask = async (taskId: number) => {
+    try {
+      await completeTaskForToday(taskId);
+    } catch (error) {
+      console.error('Error completing task:', error);
+    }
+  };
+
+  const handleCheckboxChange = async (taskId: number) => {
+    await completeTask(taskId);
+    loadTasks();
+  };
+
   return (
-      <div className="flex flex-col items-end mt-5">
+    <div className="flex flex-col items-end mt-5">
+      <div className="flex w-full justify-between">
+        <H1>All Task</H1>
+        <div className="flex items-center gap-3">
+          <Link href="/add-task">
+            <Button className="bg-all-task-add-button-bg text-all-task-add-button-text border-2 border-all-task-add-button-border">
+              <span className="">+ </span>Add Task
+            </Button>
+          </Link>
 
-        <div className="flex w-full justify-between">
-          <H1>All Task</H1>
-          <div className="flex items-center gap-3">
-            <Link href="/add-task">
-              <Button className="bg-all-task-add-button-bg text-all-task-add-button-text border-2 border-all-task-add-button-border">
-                <span className="">+ </span>Add Task
-              </Button>
-            </Link>
-
-            {!isDeleteTriggered ? (
-              <Button
-                variant="outline"
-                className="bg-all-task-delete-button-bg text-white border-2"
-                onClick={toggleDeleteTrigger}
-              >
-                <Trash />
-              </Button>
-            ) : (
-              <Button
-                className="bg-all-task-delete-button-bg text-white border-2"
-                onClick={() => setDialogOpen(true)}
-              >
-                Delete Task
-              </Button>
-            )}
-          </div>
+          {!isDeleteTriggered ? (
+            <Button
+              variant="outline"
+              className="bg-all-task-delete-button-bg text-white border-2"
+              onClick={toggleDeleteTrigger}
+            >
+              <Trash />
+            </Button>
+          ) : (
+            <Button
+              className="bg-all-task-delete-button-bg text-white border-2"
+              onClick={() => setDialogOpen(true)}
+            >
+              Delete Task
+            </Button>
+          )}
         </div>
-
-        <DeleteDialog isDialogOpen={isDialogOpen} setDialogOpen={setDialogOpen} onClose={handleDialogClose}/>
-        <TaskCard tasks={taskList} />
-
       </div>
-    );
+
+      <DeleteDialog
+        isDialogOpen={isDialogOpen}
+        setDialogOpen={setDialogOpen}
+        onClose={handleDialogClose}
+      />
+      <TaskCard tasks={taskList} handleCheckboxChange={handleCheckboxChange} />
+    </div>
+  );
 }
