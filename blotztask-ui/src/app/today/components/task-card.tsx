07334be7965b5af908d347b2f874c069d9a12fc5
benjaminneoh <benjaminneoh6343@gmail.com>
2025-01-11 18:54:01 +1100

    remove unused div

--- blotztask-ui/src/app/today/components/task-card.tsx
+++ blotztask-ui/src/app/today/components/task-card.tsx
@@ -13,20 +13,18 @@ const TaskCard = <T,>({
   handleCheckboxChange,
 }: TaskCardProps<T>) => {
   return (
-    <div className="w-full">
-      <div className="flex flex-row space-x-4">
-        <div className="flex flex-row justify-start items-center pr-6 space-x-4  border-r-4 border-primary">
-          {handleCheckboxChange && taskId ? (
-            <Checkbox
-              onCheckedChange={() => handleCheckboxChange(taskId)}
-              className="h-6 w-6 mr-6 rounded-full border-2 border-black"
-            />
-          ) : (
-            <div className="w-6 h-6 mr-6 border border-gray-400 rounded-full border-dashed"></div>
-          )}
-        </div>
-        {children}
+    <div className="flex flex-row space-x-4">
+      <div className="flex flex-row justify-start items-center pr-6 space-x-4  border-r-4 border-primary">
+        {handleCheckboxChange && taskId ? (
+          <Checkbox
+            onCheckedChange={() => handleCheckboxChange(taskId)}
+            className="h-6 w-6 mr-6 rounded-full border-2 border-black"
+          />
+        ) : (
+          <div className="w-6 h-6 mr-6 border border-gray-400 rounded-full border-dashed"></div>
+        )}
       </div>
+      {children}
     </div>
   );
 };
