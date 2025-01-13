c9ee66f409ce0063d8e1199710fda0ab4a1c79c8
nxn-nicole <nxnnxn2@163.com>
2025-01-13 20:08:51 +1100

    solve merge conflicts

--- blotztask-ui/src/app/today/components/task-content.tsx
+++ blotztask-ui/src/app/today/components/task-content.tsx
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-import { TaskDTO } from '../models/task-dto';
-import DueDayTag from './due-day-tag';
-import TaskSeparator from '../shared/task-separator';
-=======
-import { Separator } from '@/components/ui/separator';
 import { TaskDetailDTO } from '../../task-list/models/task-detail-dto';
 import DueDateTag from './due-date-tag';
->>>>>>> 6e3e7f1f0510b5533d4a7e51e74971e8d11f79a5
+import TaskSeparator from '../shared/task-separator';
 
 export default function TaskContent({ task }: { task: TaskDetailDTO }) {
   return (
@@ -16,11 +10,7 @@ export default function TaskContent({ task }: { task: TaskDetailDTO }) {
       <div className="flex flex-col w-full bg-transparent px-6">
         <div className="flex flex-row justify-between w-full">
           <p className="font-bold">{task?.title}</p>
-<<<<<<< HEAD
-          <DueDayTag task={task} />
-=======
           <DueDateTag task={task} />
->>>>>>> 6e3e7f1f0510b5533d4a7e51e74971e8d11f79a5
         </div>
 
         <div className="flex w-full text-sm text-gray-500 mt-2">
