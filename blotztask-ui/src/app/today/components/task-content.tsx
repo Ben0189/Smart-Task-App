5a28ffae33835fdbeed2999187102f5fd3795522
nxn-nicole <nxnnxn2@163.com>
2025-01-13 18:48:55 +1100

    Add color changing separator

--- blotztask-ui/src/app/today/components/task-content.tsx
+++ blotztask-ui/src/app/today/components/task-content.tsx
@@ -1,26 +1,35 @@
+import { Separator } from '@/components/ui/separator';
 import { TaskDTO } from '../schema/schema';
 
 export default function TaskContent({ task }: { task: TaskDTO }) {
   return (
-    <div
-      className="flex flex-col w-full bg-transparent px-6 border-l-4 border-primary">
-      <div>
-        <p className="font-bold">{task?.title}</p>
-      </div>
+    <div className="flex flex-row w-full bg-transparent">
+      <Separator
+        orientation="vertical"
+        decorative={true}
+        className="w-[4px]"
+        style={{ backgroundColor: task.label.color || 'gray' }}
+      />
 
-      <div className="flex w-full text-sm text-gray-500 mt-2">
-        <div className="flex flex-col w-full">
-          <p>{task?.description}</p>
+      <div className="flex flex-col w-full bg-transparent px-6">
+        <div>
+          <p className="font-bold">{task?.title}</p>
         </div>
 
-        <div className="flex items-start ml-4 w-32">
-          <div
-            className="h-4 w-4 rounded-full"
-            style={{ backgroundColor: task.label.color || 'gray' }}
-          ></div>
-          <span className="ml-2 font-bold">
-            {task.label?.name || 'No label name'}
-          </span>
+        <div className="flex w-full text-sm text-gray-500 mt-2">
+          <div className="flex flex-col w-full">
+            <p>{task?.description}</p>
+          </div>
+
+          <div className="flex items-start ml-4 w-32">
+            <div
+              className="h-4 w-4 rounded-full"
+              style={{ backgroundColor: task.label.color || 'gray' }}
+            ></div>
+            <span className="ml-2 font-bold">
+              {task.label?.name || 'No label name'}
+            </span>
+          </div>
         </div>
       </div>
     </div>
