f7dce15451edb3285dcfa4a2a7f4afb251fb7592
insidethewall <rxy550569417@gmail.com>
2025-01-16 22:14:14 +1000

    feat: removing the icons and change to react-icon

--- blotztask-ui/src/app/today/components/add-task-card.tsx
+++ blotztask-ui/src/app/today/components/add-task-card.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import AddTaskForm from './add-task-form';
-import Icon from '@/components/ui/icon';
+import { PlusIcon } from '@radix-ui/react-icons';
 
 const AddTaskCard = ({ onAddTask }) => {
   const [isFormVisible, setIsFormVisible] = useState(false);
@@ -15,8 +15,7 @@ const AddTaskCard = ({ onAddTask }) => {
       <div className="flex items-center gap-2 cursor-pointer" onClick={() => setIsFormVisible(true)}>
         {!isFormVisible ? (
           <>
-            <Icon name="add" size="md" className="fill-blue-400" />
-            {/* <PlusIcon className="w-6 h-6 text-blue-400" /> */}
+            <PlusIcon className="w-6 h-6 text-blue-400" />
             <span className="text-blue-400 font-semibold text-lg">Add a task</span>
           </>
         ) : (
--- blotztask-ui/src/app/today/components/add-task-form.tsx
+++ blotztask-ui/src/app/today/components/add-task-form.tsx
@@ -3,6 +3,14 @@ import { useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 
+// import { format } from 'date-fns';
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
+import { Calendar } from '@/components/ui/calendar';
+import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { CalendarIcon } from '@radix-ui/react-icons';
+import { IoPricetagOutline } from 'react-icons/io5';
+
 const taskSchema = z.object({
   title: z.string(), // Simple string without validation rules
   description: z.string().optional(),
@@ -14,26 +22,63 @@ const AddTaskForm = ({ onSubmit }) => {
     resolver: zodResolver(taskSchema),
     defaultValues: {
       title: '',
+      description: '',
+      date: null,
     },
   });
 
   const handleFormSubmit = (data) => {
-    onSubmit(data.title); // Pass task title to parent
+    onSubmit(data); // Pass task title to parent
     reset();
   };
 
+  const [date, setDate] = React.useState<Date>();
   return (
     <form className="flex flex-col space-y-2" onSubmit={handleSubmit(handleFormSubmit)}>
-      <input
-        type="text"
-        className="border p-2 rounded-md border-gray-300"
-        placeholder="Enter task title"
-        {...register('title')} // React Hook Form integration
-      />
-      <div className="flex space-x-2">
-        <button type="submit" className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">
-          Add Task
-        </button>
+      <div className="flex flex-col space-y-0 ">
+        <input
+          type="text"
+          className="border-0 font-bold p-2 focus:ring-0 focus:outline-none"
+          placeholder="Enter task title"
+          {...register('title')} // React Hook Form integration
+        />
+
+        <input
+          type="text"
+          className="border-0 p-2 text-sm focus:ring-0 focus:outline-none"
+          placeholder="Fill in the detailed information"
+          {...register('description')}
+        />
+      </div>
+      <div className="flex items-center space-x-4">
+        <Popover>
+          <PopoverTrigger asChild>
+            <Button
+              variant={'secondary'}
+              className={cn(
+                'bg-gray-100 text-gray-600 px-4 py-2 rounded-full hover:bg-gray-200'
+                // 'w-[160px] justify-start text-left font-normal',
+                // !date && 'text-muted-foreground'
+              )}
+            >
+              <CalendarIcon className="fill-gray-600  mr-2" />
+              <span>Add Date</span>
+              {/* <CalendarIcon />
+              {date ? format(date, 'PPP') : <span>Add Date</span>} */}
+            </Button>
+          </PopoverTrigger>
+          <PopoverContent className="w-auto p-0">
+            <Calendar mode="single" selected={date} onSelect={setDate} initialFocus />
+          </PopoverContent>
+        </Popover>
+        <Button
+          variant="secondary"
+          className="bg-gray-100 text-gray-600 px-4 py-2 rounded-full hover:bg-gray-200"
+        >
+          {/* <Icon name="sell" size="md" className="fill-gray-600 origin-center rotate-90 mr-2" /> */}
+          <IoPricetagOutline className="fill-gray-600 mr-2" />
+          <span>Add Label</span>
+        </Button>
       </div>
     </form>
   );
