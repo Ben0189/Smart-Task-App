5102d0a1ea3cb855def9c754c9794745bf12176a
CharliePeng <charliepeng516@gmail.com>
2025-01-24 22:15:24 +1100

    Add UserInfoDTO.cs for user info service and controller

---
+++ blotztask-api/Models/UserInfoDTO.cs
@@ -0,0 +1,9 @@
+namespace BlotzTask.Models
+{
+    public class UserInfoDTO
+    {
+        public string UserName { get; set; } = string.Empty;
+        
+        public string Email {get; set;} = string.Empty;
+    }
+}
