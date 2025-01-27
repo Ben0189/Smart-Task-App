c8e3b3c77ea6daa4dcc08cd4896286e1f51cee0b
nxn-nicole <nxnnxn2@163.com>
2025-01-27 22:08:00 +1100

    manage click state of label button

--- blotztask-ui/src/app/dashboard/today/shared/label-group.tsx
+++ blotztask-ui/src/app/dashboard/today/shared/label-group.tsx
@@ -1,30 +1,40 @@
 import { useState } from 'react';
 
-const LabelGroupButton = ({ LabelColor, LabelText, className = '' }) => {
-  const [isClicked, setIsClicked] = useState(false);
-
-  const handleClick = () => {
-    setIsClicked(!isClicked);
-  };
-
+const LabelGroupButton = ({ LabelColor, LabelText, className = '', isActive, onClick }) => {
   return (
     <button
-      className={`flex flex-row px-4 py-2 ${isClicked ? 'bg-primary' : 'bg-white'} ${className}`}
-      onClick={handleClick}
+      className={`flex flex-row px-4 py-2 ${isActive ? 'bg-primary' : 'bg-white'} ${className}`}
+      onClick={onClick}
     >
       <div className={`h-4 w-4 rounded-full ${LabelColor} mr-2`}></div>
-      <span className={`${isClicked ? 'text-white' : 'text-gray-500'}`}>{LabelText}</span>
+      <span className={`${isActive ? 'text-white' : 'text-gray-500'}`}>{LabelText}</span>
     </button>
   );
 };
 
 const LabelGroup = () => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const handleButtonClick = (index) => {
+    setActiveIndex(index);
+  };
   return (
-    <div className="flex flex-col text-xs place-items-center bg-transparent rounded-2xl">
-      <LabelGroupButton LabelColor="bg-amber-400" LabelText="Personal" className="rounded-t-lg" />
-      <LabelGroupButton LabelColor="bg-rose-500" LabelText="Personal" />
-      <LabelGroupButton LabelColor="bg-cyan-300" LabelText="Personal" />
-      <LabelGroupButton LabelColor="bg-blue-700" LabelText="Personal" className="rounded-b-lg" />
+    <div className="flex flex-col text-xs bg-transparent rounded-2xl">
+      {[
+        { color: 'bg-amber-400', text: 'Personal', className: 'rounded-t-lg' },
+        { color: 'bg-rose-500', text: 'Academic' },
+        { color: 'bg-cyan-300', text: 'Others' },
+        { color: 'bg-blue-700', text: 'Work', className: 'rounded-b-lg' },
+      ].map((item, index) => (
+        <LabelGroupButton
+          key={index}
+          LabelColor={item.color}
+          LabelText={item.text}
+          className={item.className}
+          isActive={activeIndex === index}
+          onClick={() => handleButtonClick(index)}
+        />
+      ))}
     </div>
   );
 };
