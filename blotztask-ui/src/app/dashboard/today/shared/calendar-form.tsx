423acb499913ef57cb2182d68b2a1dd360d484eb
nxn-nicole <nxnnxn2@163.com>
2025-01-30 15:58:08 +1100

    change calendar date limit

--- blotztask-ui/src/app/dashboard/today/shared/calendar-form.tsx
+++ blotztask-ui/src/app/dashboard/today/shared/calendar-form.tsx
@@ -50,13 +50,7 @@ export function CalendarForm({ task }) {
                 </FormControl>
               </PopoverTrigger>
               <PopoverContent className="w-auto p-0" align="start" onCloseAutoFocus={handleCalendarClose}>
-                <Calendar
-                  mode="single"
-                  selected={field.value}
-                  onSelect={field.onChange}
-                  disabled={(date) => date > new Date() || date < new Date('1900-01-01')}
-                  initialFocus
-                />
+                <Calendar mode="single" selected={field.value} onSelect={field.onChange} initialFocus />
               </PopoverContent>
             </Popover>
           </FormItem>
