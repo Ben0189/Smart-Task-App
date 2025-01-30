1d54e120112c994f2fd1886920218444f5791140
nxn-nicole <nxnnxn2@163.com>
2025-01-30 15:43:48 +1100

    Update label-select; update calendar to calendar form

--- blotztask-ui/src/app/dashboard/today/components/task-content.tsx
+++ blotztask-ui/src/app/dashboard/today/components/task-content.tsx
@@ -3,16 +3,14 @@ import TaskSeparator from '../shared/task-separator';
 import { Pencil, Trash2 } from 'lucide-react';
 import { useState } from 'react';
 import { TaskDetailDTO } from '@/app/dashboard/task-list/models/task-detail-dto';
-import PopoverCalendar from '../shared/popover-calendar';
 import { Textarea } from '@/components/ui/textarea';
 import { Input } from '@/components/ui/task-card-input';
 import { LabelSelect } from '../shared/label-select';
+import { CalendarForm } from '../shared/calendar-form';
 
 export default function TaskContent({ task }: { task: TaskDetailDTO }) {
   const [isEditing, setIsEditing] = useState(false);
   const handleEditState = () => setIsEditing(!isEditing);
-  // const [showLabel, setShowLabel] = useState(false);
-  // const handleLabelClose = () => setShowLabel(false);
 
   return (
     <div className="flex flex-col w-full">
@@ -61,25 +59,8 @@ export default function TaskContent({ task }: { task: TaskDetailDTO }) {
           {isEditing && (
             <div className="flex flex-row inline-block justify-between mt-4 mb-2">
               <div className="flex flex-row items-center">
-                <PopoverCalendar task={task} />
-
-                {/* <Popover>
-                  <PopoverTrigger>
-                    <button
-                      className={`flex flex-row
-                                items-center mr-4 rounded-full px-3 py-1 
-                                ${showLabel ? 'bg-primary text-white' : 'bg-gray-300 text-neutral-700'}`}
-                      onClick={() => setShowLabel((prev) => !prev)}
-                    >
-                      <Tag className="mr-1" size={16} />
-                      <span className="text-xs">{task.label?.name || 'No label name'}</span>
-                    </button>
-                  </PopoverTrigger>
-                  <PopoverContent onCloseAutoFocus={handleLabelClose} className="w-30 rounded-lg p-0">
-                    <SelectLabel />
-                  </PopoverContent>
-                </Popover> */}
-                <LabelSelect/>
+                <CalendarForm task={task} />
+                <LabelSelect />
               </div>
               <div className="flex flex-row ">
                 <button
--- blotztask-ui/src/app/dashboard/today/shared/label-select.tsx
+++ blotztask-ui/src/app/dashboard/today/shared/label-select.tsx
@@ -1,24 +1,23 @@
-import * as React from "react"
+import * as React from 'react';
 
 import {
-    LabelSelectItem,
+  LabelSelectItem,
   Select,
   SelectContent,
   SelectGroup,
   SelectLabelTrigger,
   SelectValue,
-} from "@/components/ui/select"
-import { Tag } from "lucide-react"
-import { LabelDTO } from "@/model/label-dto";
-
+} from '@/components/ui/select';
+import { Tag } from 'lucide-react';
+import { LabelDTO } from '@/model/label-dto';
 
 export function LabelSelect() {
-    const labels: LabelDTO[] = [
-        { id: 1, name: 'Personal', color: 'bg-amber-400' },
-        { id: 2, name: 'Academic', color: 'bg-rose-500' },
-        { id: 3, name: 'Others', color: 'bg-cyan-300' },
-        { id: 4, name: 'Work', color: 'bg-blue-700' },
-    ];
+  const labels: LabelDTO[] = [
+    { id: 1, name: 'Personal', color: 'bg-amber-400' },
+    { id: 2, name: 'Academic', color: 'bg-rose-500' },
+    { id: 3, name: 'Others', color: 'bg-cyan-300' },
+    { id: 4, name: 'Work', color: 'bg-blue-700' },
+  ];
 
   return (
     <Select>
@@ -27,21 +26,21 @@ export function LabelSelect() {
         <SelectValue placeholder="Select Label" />
       </SelectLabelTrigger>
       <SelectContent>
-      <SelectGroup>
+        <SelectGroup>
           {labels.map((label) => (
             <LabelSelectItem
               key={label.id}
               value={label.name}
               className="flex flex-rowitems-center px-2 py-1"
             >
-                <div className="flex flex-row">
-                    <div className={`h-4 w-4 rounded-full mr-2 ${label.color}`}></div>
-                    {label.name}
-                </div>
+              <div className="flex flex-row">
+                <div className={`h-4 w-4 rounded-full mr-2 ${label.color}`}></div>
+                {label.name}
+              </div>
             </LabelSelectItem>
           ))}
         </SelectGroup>
       </SelectContent>
     </Select>
-  )
+  );
 }
