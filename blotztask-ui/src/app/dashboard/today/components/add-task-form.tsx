94852f54ffaf02fd323870e1f5965aba4ab984af
nxn-nicole <nxnnxn2@163.com>
2025-01-30 18:53:18 +1100

    Refactor the date picker

--- blotztask-ui/src/app/dashboard/today/components/add-task-form.tsx
+++ blotztask-ui/src/app/dashboard/today/components/add-task-form.tsx
@@ -10,6 +10,7 @@ import { CalendarIcon } from '@radix-ui/react-icons';
 import { Tag } from 'lucide-react';
 import { Input } from '@/components/ui/task-card-input';
 import { Textarea } from '@/components/ui/textarea';
+import { CalendarForm } from '../shared/calendar-form';
 
 const taskSchema = z.object({
   title: z.string(), // Simple string without validation rules
@@ -44,20 +45,8 @@ const AddTaskForm = ({ onSubmit }) => {
         />
       </div>
       <div className="flex items-center space-x-4">
-        <Popover>
-          <PopoverTrigger asChild>
-            <Button
-              variant={'secondary'}
-              className={cn('bg-gray-100 text-gray-600 px-4 py-2 rounded-full hover:bg-gray-200')}
-            >
-              <CalendarIcon className="fill-gray-600  mr-2" />
-              <span>Add Date</span>
-            </Button>
-          </PopoverTrigger>
-          <PopoverContent className="w-auto p-0">
-            <Calendar mode="single" selected={date} onSelect={setDate} initialFocus />
-          </PopoverContent>
-        </Popover>
+        <CalendarForm></CalendarForm>
+
         <Button
           variant="secondary"
           className="bg-gray-100 text-gray-600 px-4 py-2 rounded-full hover:bg-gray-200"
