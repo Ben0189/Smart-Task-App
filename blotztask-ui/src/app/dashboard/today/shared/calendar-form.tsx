04046d81d30f7abebae5b2c8a6836eaf2e9eceb5
CharliePeng <charliepeng516@gmail.com>
2025-02-01 19:35:08 +1100

    Deal with time when two refs are not provided from parent component

--- blotztask-ui/src/app/dashboard/today/shared/calendar-form.tsx
+++ blotztask-ui/src/app/dashboard/today/shared/calendar-form.tsx
@@ -17,7 +17,13 @@ const FormSchema = z.object({
   }),
 });
 
-export function CalendarForm({ task, datePickerRef }: { task?: TaskDetailDTO, datePickerRef: React.RefObject<HTMLDivElement> }) {
+export function CalendarForm({
+  task,
+  datePickerRef,
+}: {
+  task?: TaskDetailDTO;
+  datePickerRef?: React.RefObject<HTMLDivElement>;
+}) {
   const [showCalendar, setShowCalendar] = useState(false);
   const handleCalendarClose = () => setShowCalendar(false);
 
@@ -26,7 +32,7 @@ export function CalendarForm({ task, datePickerRef }: { task?: TaskDetailDTO, da
   });
 
   return (
-    <Form {...form} >
+    <Form {...form}>
       <FormField
         control={form.control}
         name="deadline"
@@ -52,7 +58,12 @@ export function CalendarForm({ task, datePickerRef }: { task?: TaskDetailDTO, da
                   </button>
                 </FormControl>
               </PopoverTrigger>
-              <PopoverContent ref={datePickerRef} className="w-auto p-0" align="start" onCloseAutoFocus={handleCalendarClose}>
+              <PopoverContent
+                ref={datePickerRef?.current ? datePickerRef : undefined}
+                className="w-auto p-0"
+                align="start"
+                onCloseAutoFocus={handleCalendarClose}
+              >
                 <Calendar mode="single" selected={field.value} onSelect={field.onChange} initialFocus />
               </PopoverContent>
             </Popover>
