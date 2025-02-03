ed6307f59a9cb542daf43f0dfbf914a085eafae8
benjaminneoh <benjaminneoh6343@gmail.com>
2025-02-03 21:11:34 +1100

    initial form setup for title field

--- blotztask-ui/src/app/dashboard/today/components/add-task-form.tsx
+++ blotztask-ui/src/app/dashboard/today/components/add-task-form.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import { Controller, SubmitHandler, useForm } from 'react-hook-form';
+import { SubmitHandler, useForm } from 'react-hook-form';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Input } from '@/components/ui/task-card-input';
 import { Textarea } from '@/components/ui/textarea';
-import { CalendarForm } from '../shared/calendar-form';
-import { Form } from '@/components/ui/form';
+import { Form, FormControl, FormField, FormItem, FormMessage } from '@/components/ui/form';
 // import { LabelSelect } from '../shared/label-select';
 
 const taskSchema = z.object({
   title: z.string().min(1, 'Title is required'),
-  description: z.string().optional(),
-  date: z.date(),
+  // description: z.string().optional(),
+  // date: z.date(),
 });
 
 type FormField = z.infer<typeof taskSchema>;
@@ -21,33 +20,47 @@ const AddTaskForm = () => {
     resolver: zodResolver(taskSchema),
     defaultValues: {
       title: '',
-      description: '',
-      date: undefined,
+      // description: '',
+      // date: undefined,
     },
   });
 
   const handleAddTask: SubmitHandler<FormField> = async (data) => {
-    try {
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-      console.log(data);
-    } catch (error) {}
+    // await new Promise((resolve) => setTimeout(resolve, 1000));
+    console.log(data);
   };
 
   return (
-    <form className="flex flex-col w-full space-y-2">
-      <div className="flex flex-col w-full">
-        <Input placeholder="Enter task title" className="font-bold text-base" />
-        <Textarea placeholder="Fill in the detailed information" className="w-full" />
-      </div>
-      <div className="flex items-center">
-        {/* <CalendarForm>
-            <span>Add Date</span>
-          </CalendarForm> */}
+    <Form {...form}>
+      <form 
+        className="flex flex-col w-full space-y-2"
+        onSubmit={form.handleSubmit(handleAddTask)}
+      >
+        <div className="flex flex-col w-full">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input placeholder="Enter task title" className="font-bold text-base" {...field}></Input>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Textarea placeholder="Fill in the detailed information" className="w-full" />
+        </div>
+        <div className="flex items-center">
+          {/* <CalendarForm>
+              <span>Add Date</span>
+            </CalendarForm> */}
 
-        {/* <LabelSelect></LabelSelect> */}
-      </div>
-      <button type="submit">Sumbit</button>
-    </form>
+          {/* <LabelSelect></LabelSelect> */}
+        </div>
+        <button type="submit">Test Submit</button>
+      </form>  
+    </Form>
   );
 };
 
